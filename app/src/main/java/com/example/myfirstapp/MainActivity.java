c1cc89c261cff8172f14e24cb47c151db6554f38
Icet0 <49767388+Icet0@users.noreply.github.com>
2020-03-17 11:19:18 +1100

    succes

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,13 +18,22 @@ public class MainActivity extends AppCompatActivity {
 
     TextView barcodeResult;
 
+    private static final int RC_BARCODE_CAPTURE = 9001;
+    private static final String TAG = "BarcodeMain";
+
+    private TextView barcodeValue;
+
+
+
+
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        barcodeResult = (TextView)findViewById(R.id.barcode_result);
+        barcodeValue = (TextView)findViewById(R.id.barcode_value);
+//        barcodeResult = (TextView)findViewById(R.id.barcode_result);
     }
 
     /** Called when the user taps the Send button */
@@ -44,26 +54,38 @@ public class MainActivity extends AppCompatActivity {
 
     public void scanBarcode(View v){
 
-        Intent intent = new Intent(this, BarcodeReaderActivity.class);
-        startActivityForResult(intent, 0);
+//        Intent intent = new Intent(this, BarcodeReaderActivity.class);
+//        startActivityForResult(intent, 0);
+        Intent intent = new Intent(this, BarcodeCaptureActivity.class);
+        startActivityForResult(intent, RC_BARCODE_CAPTURE);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-         if(requestCode==0){
-             if(resultCode== CommonStatusCodes.SUCCESS){
-                 if(data!=null){
-                     Barcode barcode = data.getParcelableExtra("barcode");
-                     barcodeResult.setText("Barcode value :"+barcode.displayValue);
-                 }
-                 else{
-                     barcodeResult.setText("No Barcode found");
-                 }
-             }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    barcodeValue.setText(barcode.displayValue);
+                    Log.d(TAG, "Barcode read: " + barcode.displayValue);
 
-         }
-         else {
-             super.onActivityResult(requestCode, resultCode, data);
-         }
+
+
+                    Intent intent = new Intent(this, FirstView.class);
+                    startActivity(intent);
+
+
+
+                } else {
+
+                    Log.d(TAG, "No barcode captured, intent data is null");
+                }
+            } else {
+                        CommonStatusCodes.getStatusCodeString(resultCode);
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
