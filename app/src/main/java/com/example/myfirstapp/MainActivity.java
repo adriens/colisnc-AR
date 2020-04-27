50578b20728ac50e2c65a15b7b0faccc1201d163
Icet0 <49767388+Icet0@users.noreply.github.com>
2020-04-27 18:22:49 +1100

    TextView_setText

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.myfirstapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.LinkedList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView barcodeResult;
+
+    private static final int RC_BARCODE_CAPTURE = 9001;
+    private static final String TAG = "BarcodeMain";
+
+    private TextView barcodeValue;
+
+
+
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        barcodeValue = (TextView)findViewById(R.id.barcode_value);
+//        barcodeResult = (TextView)findViewById(R.id.barcode_result);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void NextView(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, FirstView.class);
+        startActivity(intent);
+    }
+
+    public void scanBarcode(View v){
+
+//        Intent intent = new Intent(this, BarcodeReaderActivity.class);
+//        startActivityForResult(intent, 0);
+        Intent intent = new Intent(this, BarcodeCaptureActivity.class);
+        startActivityForResult(intent, RC_BARCODE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    barcodeValue.setText(barcode.displayValue);
+                    Log.d(TAG, "Barcode read: " + barcode.displayValue);
+
+
+                    String resultat = barcode.displayValue;
+                    System.out.println("--------------------------------------------------------------------RESULTAT :"+resultat+"----------------------------------------------------------------------\n");
+                    Intent intent = new Intent(this, FirstView.class);
+                    intent.putExtra("resultat",resultat);
+                    startActivity(intent);
+
+
+
+                } else {
+
+                    Log.d(TAG, "No barcode captured, intent data is null");
+                }
+            } else {
+                CommonStatusCodes.getStatusCodeString(resultCode);
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapp/ScanBarcodeActivity.java
@@ -0,0 +1,89 @@
+package com.example.myfirstapp;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class ScanBarcodeActivity extends Activity {
+
+    SurfaceView cameraPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_barcode);
+
+        cameraPreview = findViewById(R.id.camera_preview);
+        createCameraSource();
+    }
+
+    private void createCameraSource(){
+
+        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).build();
+        final CameraSource cameraSource = new CameraSource.Builder(this,barcodeDetector)
+                .setAutoFocusEnabled(true)
+                .setRequestedPreviewSize(1600,1024)
+                .build();
+
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+                if (ActivityCompat.checkSelfPermission(ScanBarcodeActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+
+                    return;
+                }
+                try {
+                    cameraSource.start(cameraPreview.getHolder());
+                } catch (IOException e) {
+                    System.err.println("PROBLEM CAMERA START");
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if(barcodes.size()>0){
+                    Intent intent=new Intent();
+                    intent.putExtra("barcode",barcodes.valueAt(0));//get latest barcode from the array
+                    setResult(CommonStatusCodes.SUCCESS,intent);
+                    finish();
+                }
+            }
+        });
+    }
+}
